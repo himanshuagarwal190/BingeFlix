94707a208124fd483babd820a82d4288a1d7230f
Himanshu Agarwal <himanshuagarwal@MB-M-HIMANSHUA.local>
2024-02-26 01:38:01 +0530

    intersection observer

--- components/Tiles.jsx
+++ components/Tiles.jsx
@@ -1,61 +1,69 @@
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import config from '../config'
 import Link from 'next/link'
 
 export default function Tiles({ selection, movieData, setMovieData, genreSelected }){
-    let scrollCount = 1
+    const pageNumber = useRef(0)
+    const loading = useRef(false)
+
+    const lastTileRef = useRef()
+    const refCallback = useCallback(node =>{
+        if(!node) return
+        if(loading.current) return
+        if(movieData.length == 0) return
+        if(lastTileRef.current) lastTileRef.current.disconnect()
+        lastTileRef.current = new IntersectionObserver(entries =>{
+            if(entries[0].isIntersecting){
+                pageNumber.current += 1
+                getMovies(pageNumber.current)
+            }
+        })
+        lastTileRef.current.observe(node)
+
+    })
 
     useEffect(()=>{
         setMovieData([])
-        scrollCount = 1
-        getMovies(scrollCount)
-        window.addEventListener("scroll", onPageScroll);
-        return () => window.removeEventListener("scroll", onPageScroll)
+        pageNumber.current = 1
+        getMovies(pageNumber.current)
     },[selection, genreSelected])
 
     async function getMovies(pageCount){
         try {
+            loading.current = true
             const url = config.movieDBAPIUrl + '/trending/' + selection + '/week?api_key=' + config.movieDBKey + '&page=' + pageCount
             const response = await axios.get(url)
+            let movieArr = []
             if(genreSelected != 0){
-                const movieArr = response.data.results.filter(movie  =>{
+                movieArr = response.data.results.filter(movie  =>{
                     if(movie.genre_ids.includes(genreSelected)) return movie
                 })
-                setMovieData(prevState => [...prevState, ...movieArr])
             } else {
-                setMovieData(prevState => [...prevState, ...response.data.results])
+                movieArr = response.data.results
             }
-            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                scrollCount++
-                getMovies(scrollCount)
-             } else {
-                 return
-             }
+            movieArr = [...movieData, ...movieArr]
+            setMovieData(movieArr)
+            loading.current = false
         } catch(error){
             console.log(error)
         }
     }
-    function onPageScroll(){
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            scrollCount++
-            getMovies(scrollCount)
-         }
-         
-    }
-
     
     return (
-        <div onClick={onPageScroll} className="flex col-gap-30 row-gap-30 flex-wrap mt-10">
+        <div className="flex col-gap-30 row-gap-30 flex-wrap mt-10">
             {movieData.length > 0 ? movieData.map((data, idx) => (
-                <Link key={idx} href={'watch/' + selection + '_' + data.id}>
-                    <a className="relative cursor-pointer">
-                        <img className="tile-box" src={'https://image.tmdb.org/t/p/original/' + data.poster_path} alt="movie-tile" />
-                        <div className='year-badge'>{new Date(data.release_date ? data.release_date : data.first_air_date).getFullYear()}</div>
-                        <div className='rating'>{data.vote_average}</div>
-                        <div className='movie-title'>{data.title ? data.title : data.original_name}</div>
-                    </a>
-                </Link>
+                <div className="relative cursor-pointer" ref={idx === movieData.length - 10 ? refCallback : null} key={idx}>
+                    <Link href={'watch/' + selection + '_' + data.id}>
+                        <a>
+                            <img className="tile-box" src={'https://image.tmdb.org/t/p/original/' + data.poster_path} alt="movie-tile" />
+                            <div className='year-badge'>{new Date(data.release_date ? data.release_date : data.first_air_date).getFullYear()}</div>
+                            <div className='rating'>{data.vote_average}</div>
+                            <div className='movie-title'>{data.title ? data.title : data.original_name}</div>
+                        </a>
+                    </Link>
+                </div>
+                
                 
             )): <div>Loading...</div>}
         </div>
